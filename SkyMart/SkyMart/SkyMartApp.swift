23000630f81db5bdaaf0019805a9e192b5382095
AnastasiiaLarina <laranweb@gmail.com>
2024-05-30 14:58:14 +0500

    start progect

---
+++ SkyMart/SkyMart/SkyMartApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkyMartApp.swift
+//  SkyMart
+//
+//  Created by Анастасия Ларина on 30.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct SkyMartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
