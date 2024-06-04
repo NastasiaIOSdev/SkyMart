c4cd5c1f445214f3a939d660e02d0dc7eeec9af9
AnastasiiaLarina <laranweb@gmail.com>
2024-06-04 13:28:02 +0500

    code refactoring

---
+++ CoordinatorMVVMSwiftUI/CoordinatorMVVMSwiftUI/CoordinatorMVVMSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoordinatorMVVMSwiftUIApp.swift
+//  CoordinatorMVVMSwiftUI
+//
+//  Created by Анастасия Ларина on 04.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct CoordinatorMVVMSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
